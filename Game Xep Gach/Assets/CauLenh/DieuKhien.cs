c1e200f9ffb7175cf5e3cd6150034f188fc8f43f
chitung811 <chitung811@gmail.com>
2021-05-26 18:26:09 +0700

    khoi

--- Game Xep Gach/Assets/CauLenh/DieuKhien.cs
+++ Game Xep Gach/Assets/CauLenh/DieuKhien.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DieuKhien : MonoBehaviour
 {
-
+    float khoigach = 0;
+    public float tocdochoi = 1;
     // Update is called once per frame
     void Update()
     {
@@ -14,18 +15,23 @@ public class DieuKhien : MonoBehaviour
     void SangTrai()
     {
         transform.position += new Vector3(-1, 0, 0);
+        if(!KiemTraVuotBien()) transform.position += new Vector3(1, 0, 0);
     }
     void SangPhai()
     {
-
+        transform.position += new Vector3(1, 0, 0);
+        if (!KiemTraVuotBien()) transform.position += new Vector3(-1, 0, 0);
     }
     void Quay()
     {
-
+        transform.Rotate(0, 0, 90);
+        if (!KiemTraVuotBien()) transform.Rotate(0, 0, -90);
     }
     void RoiXuong()
     {
-
+        transform.position += new Vector3(0, -1, 0);
+        if (!KiemTraVuotBien()) transform.position += new Vector3(0, 1, 0);
+        khoigach = Time.time;
     }
 
     void KiemTraNhanPhim()
@@ -34,5 +40,27 @@ public class DieuKhien : MonoBehaviour
         {
             SangTrai();
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SangPhai();
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Quay();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time-khoigach >= tocdochoi)
+        {
+            RoiXuong();
+        }
+    }
+
+    public bool KiemTraVuotBien()
+    {
+        foreach(Transform khoigach in transform)
+        {
+            Vector2 kt = FindObjectOfType<Bien_Gioi>().Round(khoigach.position);
+            if (FindObjectOfType<Bien_Gioi>().ConTrongLuoi(kt) == false) return false;
+        }
+        return true;
     }
 }
