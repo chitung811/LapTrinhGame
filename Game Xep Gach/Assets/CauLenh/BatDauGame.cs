70febc9e18e7d675b939843f134cdbf672a3588c
trongnhan159 <63111185+trongnhan159@users.noreply.github.com>
2021-08-28 10:14:47 +0700

    man hinh dau game

---
+++ Game Xep Gach/Assets/CauLenh/BatDauGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatDauGame : MonoBehaviour
+{
+    public void ChoiMoi()
+    {
+        Debug.Log("Đang gọi Phân cảnh chính");
+        Application.LoadLevel("PhanCanhChinh");
+    }
+    public void ThoatGame()
+    {
+        Debug.Log("Yêu cầu kết thúc game");
+        Application.Quit();
+    }
+}
